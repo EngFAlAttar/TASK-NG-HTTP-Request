c323bd840f43970a51ceec937448cc646adcae50
EngFAlAttar <engfalattar@gmail.com>
2025-04-24 12:16:22 +0300

    created pet service

--- src/app/pages/pets/pets.component.ts
+++ src/app/pages/pets/pets.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PetsHeaderComponent } from '../../components/pets-header/pets-header.component';
 import { PetsListComponent } from '../../components/pets-list/pets-list.component';
 import { pets } from '../../../data/pets';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-pets',
@@ -14,6 +15,8 @@ export class PetsComponent {
   query = '';
   allPets = pets;
 
+  private http = inject(HttpClient);
+  
   setQuery(query: string) {
     this.query = query;
   }
